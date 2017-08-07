10ac599c6484b2ad902b9d8fc18bf39f8ac1dab8
evandroflores <eof@eof.com.br>
2017-08-07 11:54:49 +0100

    a better way to init the database

--- database/db.go
+++ database/db.go
@@ -8,16 +8,31 @@ import (
 )
 
 // DB is the orm interface to the database
-var DB, _ = xorm.NewEngine("sqlite3", "./claimr.db")
+var DB *xorm.Engine
 
-// InitDB Initialises the database tables and set debug variables
-func InitDB() {
-	log.Info("Initializing database...")
+func init() {
+	initDB("./claimr.db")
+}
+
+func initDB(dbName string) {
+	log.Infof("Initializing database [%s]...", dbName)
+
+	var err error
+	DB, err = xorm.NewEngine("sqlite3", dbName)
+
+	if err != nil {
+		panic("Couldn't open nor create database")
+	}
 
 	DB.ShowSQL(true)
 	DB.ShowExecTime(true)
 
-	tableExists, _ := DB.IsTableExist(&model.VM{})
+	tableExists, err := DB.IsTableExist(&model.VM{})
+
+	if err != nil {
+		panic("Couldn't read database")
+	}
+
 	if !tableExists {
 		log.Info("Creating tables...")
 		DB.CreateTables(&model.VM{})
