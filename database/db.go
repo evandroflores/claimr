279951adedd39b9a121e0e5c93a70a39f81c9e90
evandroflores <eof@eof.com.br>
2017-09-14 20:18:44 +0100

    Changing the database to dynamodb

--- database/db.go
+++ database/db.go
@@ -3,13 +3,22 @@ package database
 import (
 	"reflect"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/go-xorm/xorm"
 	_ "github.com/mattn/go-sqlite3" // Engine for database
-	log "github.com/sirupsen/logrus"
+
 )
 
 // DB is the orm interface to the database
 var DB DbWilson
+
+// DB2 is the single database instance
+var DB2 *dynamo.DB
+
 
 type DbWilson interface {
 	Get(bean interface{}) (bool, error)
@@ -31,12 +40,20 @@ func initDB(dbName string) {
 	xorDB, err := xorm.NewEngine("sqlite3", dbName)
 
 	if err != nil {
-		log.Fatalf("Couldn't open nor create database [%s].", dbName)
+		log.Fatalf("Couldn't open nor create database [%s]. %s", dbName, err)
 	}
 	DB = xorDB
 
 	xorDB.ShowSQL(true)
 	xorDB.ShowExecTime(true)
+
+	log.Info("Initializing database2...")
+	awsSession, err := session.NewSession()
+	if err != nil{
+		log.Fatalf("Could not create a aws connection. %s", err)
+	}
+	DB2 = dynamo.New(awsSession, &aws.Config{Region: aws.String("eu-west-1")})
+	log.Print(DB2)
 }
 
 // RegisterModel will check and create the model if does not exists on the databse.
--- model/container.go
+++ model/container.go
@@ -5,49 +5,106 @@ import (
 	"time"
 
 	"github.com/evandroflores/claimr/database"
+	"github.com/guregu/dynamo"
+	log "github.com/sirupsen/logrus"
 )
 
 // Container defines the Container information on database.
 type Container struct {
-	ID            int64
+	ID            string
 	TeamID        string
 	ChannelID     string
 	Name          string
 	InUseBy       string
 	InUseByReason string
-	UpdatedAt     time.Time `xorm:"updated"`
+	UpdatedAt     time.Time
 	CreatedByUser string
 }
 
+var containerTable dynamo.Table
+
 func init() {
-	database.RegisterModel(Container{})
+	database.DB2.CreateTable("ClaimrContainer", Container{})
+	containerTable = database.DB2.Table("ClaimrContainer")
+	log.Debug("___")
+	name := fmt.Sprintf("name%s", time.Now().UTC())
+	log.Debug(AddContainer("team", "channel", name, "owner"))
+	log.Debug("___")
+}
+
+func isValidContainerInput(teamID string, channelID string, name string) (bool, error){
+	if teamID == "" {
+		return false, fmt.Errorf("Cant' continue without a teamID. 🙄")
+	}
+
+	if channelID == "" {
+		return false, fmt.Errorf("Cant' continue without a channelID. 🙄")
+	}
+
+	if name == "" {
+		return false, fmt.Errorf("Cant' continue without a container name 🙄")
+	}
+
+	return true, nil
 }
 
 // GetContainer returns a container for teamID, channelID, and name provided
 func GetContainer(teamID string, channelID string, name string) (Container, error) {
-	if teamID == "" {
-		return Container{}, fmt.Errorf("Give me a teamID to find. 🙄")
+	valid, err := isValidContainerInput(teamID, channelID, name)
+
+	if !valid{
+		return Container{}, err
 	}
 
-	if channelID == "" {
-		return Container{}, fmt.Errorf("Give me a channelID to find. 🙄")
-	}
+	results := []Container{}
 
-	if name == "" {
-		return Container{}, fmt.Errorf("Give me a container name to find. 🙄")
-	}
-
-	container := Container{TeamID: teamID, ChannelID: channelID, Name: name}
-
-	found, err := database.DB.Get(&container)
+	err = containerTable.Scan().
+			Filter("TeamID = ? AND ChannelID = ? AND 'Name' = ? ", teamID , channelID, name).
+			All(&results)
 
 	if err != nil {
 		return Container{}, err
 	}
 
-	if !found {
-		return Container{}, fmt.Errorf("Container %s not found", name)
+	if len(results) == 0 {
+		return Container{}, nil
 	}
 
-	return container, nil
+	return results[0], nil
+}
+
+func AddContainer(teamID string, channelID string, name string, userID string) error {
+	valid, err := isValidContainerInput(teamID, channelID, name)
+
+	if !valid{
+		return err
+	}
+
+	container, err := GetContainer(teamID, channelID, name)
+	if err != nil {
+		return err
+	}
+	if container != (Container{}) {
+		return fmt.Errorf("There is a container with the same name on this channel. Try a different one. 😕")
+	}
+
+	now := time.Now().UTC()
+
+	newContainer := Container{
+						ID: fmt.Sprintf("%s.%s.%s", teamID, channelID, name),
+						TeamID: teamID,
+						ChannelID:channelID,
+						Name:name,
+						InUseBy: "",
+						InUseByReason: "",
+						UpdatedAt: now,
+						CreatedByUser: userID,
+					}
+
+	err = containerTable.Put(newContainer).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
