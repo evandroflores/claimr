032d9f7f7a812b3ce6e4533949a23230e3c80e56
evandroflores <eof@eof.com.br>
2017-09-30 22:05:44 +0100

    Adding tests to main package

--- main_test.go
+++ main_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMustHaveTokenEnv(t *testing.T) {
+	currentToken := os.Getenv("CLAIMR_TOKEN")
+	os.Unsetenv("CLAIMR_TOKEN")
+	defer func() { os.Setenv("CLAIMR_TOKEN", currentToken) }()
+
+	cmd := exec.Command("make", "run")
+	//cmd.Env = append(os.Environ(), "TEST_MUST_ENV=1")
+	err := cmd.Run()
+	fmt.Print(err.(*exec.ExitError))
+	//if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+	//	return
+	//}
+	assert.EqualError(t, err, "Claimr slack bot token unset. Set CLAIMR_TOKEN to continue.")
+}
