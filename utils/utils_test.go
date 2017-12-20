5761d2c165edc4119a409f4ec12589e4c07b6dc4
evandroflores <eof@eof.com.br>
2017-12-20 18:35:33 +0000

    Moving ternary ifelse to utils

---
+++ utils/utils_test.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfThenElseBooleanTrue(t *testing.T) {
+	theTruth := "I am the way and the truth and the life"
+	notTrue := "Fake news"
+
+	returned := IfThenElse(true, theTruth, notTrue)
+
+	assert.Equal(t, theTruth, returned)
+}
+
+func TestIfThenElseBooleanFalse(t *testing.T) {
+	theTruth := "I am the way and the truth and the life"
+	notTrue := "Fake news"
+
+	returned := IfThenElse(false, theTruth, notTrue)
+
+	assert.Equal(t, notTrue, returned)
+}
+
+func TestIfThenElseNumersTrue(t *testing.T) {
+	theTruth := "I am the way and the truth and the life"
+	notTrue := "Fake news"
+
+	returned := IfThenElse(1 == 1, theTruth, notTrue)
+
+	assert.Equal(t, theTruth, returned)
+}
+
+func TestIfThenElseNumersFalse(t *testing.T) {
+	theTruth := "I am the way and the truth and the life"
+	notTrue := "Fake news"
+
+	returned := IfThenElse(1 != 1, theTruth, notTrue)
+
+	assert.Equal(t, notTrue, returned)
+}
