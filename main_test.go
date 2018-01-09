3b8cdcc3d13e2c80799db4969a0f1a29fa2f4c36
Evandro Flores <eof@eof.com.br>
2018-01-09 02:30:49 +0000

    Main commands test

--- main_test.go
+++ main_test.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/bouk/monkey"
+	"github.com/evandroflores/claimr/cmd"
+	"github.com/evandroflores/claimr/model"
+	"github.com/shomali11/slacker"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,4 +27,29 @@ func TestEnvironmentToken(t *testing.T) {
 	patchLog := monkey.Patch(log.Fatal, mockLogFatal)
 	defer patchLog.Unpatch()
 	assert.PanicsWithValue(t, "log.Fatal called", main, "log.Fatal was not called")
+}
+
+func TestSlackerCommands(t *testing.T) {
+	var mockSlacker *slacker.Slacker
+
+	var commands []model.Command
+	patchCommand := monkey.PatchInstanceMethod(reflect.TypeOf(mockSlacker), "Command",
+		func(slacker *slacker.Slacker, usage string, description string, handler func(request *slacker.Request, response slacker.ResponseWriter)) {
+			if usage == "help" {
+				return //ignoring help slacker command
+			}
+			commands = append(commands, model.Command{Usage: usage, Description: description, Handler: handler})
+		})
+	patchListen := monkey.PatchInstanceMethod(reflect.TypeOf(mockSlacker), "Listen", func(*slacker.Slacker) error {
+		assert.CallerInfo()
+		return nil
+	})
+
+	main()
+
+	for i, command := range cmd.CommandList() {
+		assert.Equal(t, command.Description, commands[i].Description)
+	}
+	patchCommand.Unpatch()
+	patchListen.Unpatch()
 }
