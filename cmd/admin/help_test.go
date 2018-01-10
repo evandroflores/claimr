2f7b9b50d02ab2410a45d974df192347cec8cf3e
Evandro Flores <eof@eof.com.br>
2018-01-10 00:14:05 +0000

    Adding tests to subcommands

---
+++ cmd/admin/help_test.go
@@ -0,0 +1,28 @@
+package admin
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/bouk/monkey"
+	"github.com/shomali11/slacker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCallHelp(t *testing.T) {
+	var mockResponse *slacker.Response
+
+	expected := GenerateCommandHelp()
+
+	patchReply := monkey.PatchInstanceMethod(reflect.TypeOf(mockResponse), "Reply",
+		func(response *slacker.Response, msg string) {
+			assert.Equal(t, expected, msg)
+		})
+
+	_ = monkey.PatchInstanceMethod(reflect.TypeOf(mockResponse), "Typing",
+		func(response *slacker.Response) {})
+
+	subCommandHelp(nil, mockResponse)
+
+	patchReply.Unpatch()
+}
