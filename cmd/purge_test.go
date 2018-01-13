1368bcfdf5b54eb5c7b0ad1bdaaa3c551d2c12a5
Evandro Flores <eof@eof.com.br>
2018-01-13 12:49:49 +0000

    Fixing test Purge

--- cmd/purge_test.go
+++ cmd/purge_test.go
@@ -2,34 +2,21 @@ package cmd
 
 import (
 	"os"
-	"reflect"
 	"testing"
 
-	"github.com/bouk/monkey"
 	"github.com/evandroflores/claimr/database"
 	"github.com/evandroflores/claimr/model"
-	"github.com/evandroflores/slacker"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPurgeContainers(t *testing.T) {
-	var mockResponse *slacker.Response
-
 	containerName := "container-purge"
-	teamName := "TestTeam"
+	teamName := "TestPurge"
 	channelName := "TestChannel"
 	userName := os.Getenv("CLAIMR_SUPERUSER")
 
 	expected := "10 Container rows purged"
 
-	patchReply := monkey.PatchInstanceMethod(reflect.TypeOf(mockResponse), "Reply",
-		func(response *slacker.Response, msg string) {
-			assert.Equal(t, expected, msg)
-		})
-
-	_ = monkey.PatchInstanceMethod(reflect.TypeOf(mockResponse), "Typing",
-		func(response *slacker.Response) {})
-
+	mockResponse, patchReply := createMockReply(t, expected)
 	patchGetEvent := createMockEvent(t, teamName, channelName, userName)
 
 	for i := 1; i <= 10; i++ {
