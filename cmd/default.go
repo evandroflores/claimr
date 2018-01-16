223320600e6d79c8154e83173ab77fc0188968fc
Evandro Flores <eof@eof.com.br>
2018-01-16 00:42:26 +0000

    Just a little space here

--- cmd/default.go
+++ cmd/default.go
@@ -2,7 +2,7 @@ package cmd
 
 import "github.com/shomali11/slacker"
 
-//Default command will be called when a command is not recognized.
+// Default command will be called when a command is not recognized.
 func Default(request *slacker.Request, response slacker.ResponseWriter) {
 	response.Typing()
 	response.Reply("Not sure what you are asking for. Type `@claimr help` for valid commands.")
