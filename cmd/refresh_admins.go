4b444985e88d77ce94ac205d95178acb0e141d24
Evandro Flores <eof@eof.com.br>
2018-01-24 09:02:07 -0800

    Adding Refresh admins command

---
+++ cmd/refresh_admins.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/evandroflores/claimr/model"
+	"github.com/shomali11/slacker"
+)
+
+func init() {
+	Register("refresh-admins", "Reload's slack admins list. admin-only", refreshAdmins)
+}
+
+func refreshAdmins(request *slacker.Request, response slacker.ResponseWriter) {
+	response.Typing()
+
+	event := getEvent(request)
+	if !isAdmin(event.User) {
+		response.Reply(Messages["admin-only"])
+		return
+	}
+
+	bot := slacker.NewClient(os.Getenv("CLAIMR_TOKEN")) // This is terrible!
+	model.LoadAdmins(bot)
+	response.Reply(fmt.Sprintf(Messages["x-admin-loaded"], len(model.Admins)))
+}
