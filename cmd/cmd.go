436262df23df5539b35067a81bdb90531b789ffc
Evandro Flores <eof@eof.com.br>
2018-02-06 19:12:32 +0000

    Grouping errors

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -94,3 +94,13 @@ type Check struct {
 	isPositive bool
 	message    string
 }
+
+// RunChecks runs a list of checks and returns for the first error
+func RunChecks(checks []Check) error {
+	for _, check := range checks {
+		if check.isPositive {
+			return fmt.Errorf(check.message)
+		}
+	}
+	return nil
+}
