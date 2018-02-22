5605f636cdba5f351b147406c23944430a127c66
Evandro Flores <eof@eof.com.br>
2018-02-22 18:43:36 +0000

    Adding test for LoadAdminError

--- model/admin_test.go
+++ model/admin_test.go
@@ -1,12 +1,14 @@
 package model
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/bouk/monkey"
 	"github.com/nlopes/slack"
 	"github.com/shomali11/slacker"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -60,4 +62,27 @@ func TestLoadingAdmins(t *testing.T) {
 	patchNewRTM.Unpatch()
 	patchGetUsers.Unpatch()
 
+}
+
+func TestLoadingAdminsError(t *testing.T) {
+	var mockClient *slack.Client
+
+	expectedMsg := "Simulated Error"
+
+	patchGetUsers := monkey.PatchInstanceMethod(reflect.TypeOf(mockClient), "GetUsers",
+		func(*slack.Client) ([]slack.User, error) {
+			return nil, fmt.Errorf(expectedMsg)
+		})
+
+	mockLogErrorf := func(format string, args ...interface{}) {
+		assert.Equal(t, expectedMsg, fmt.Sprintf(format, args))
+	}
+
+	patchLog := monkey.Patch(log.Warnf, mockLogErrorf)
+
+	bot := slacker.NewClient("fake")
+	LoadAdmins(bot)
+
+	patchLog.Unpatch()
+	patchGetUsers.Unpatch()
 }
