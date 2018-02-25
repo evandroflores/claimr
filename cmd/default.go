a28a28a1b87e1d315ab82c509e47ae0778d0a49a
Evandro Flores <eof@eof.com.br>
2018-02-25 16:37:30 +0000

    moving cmd/default

--- cmd/default.go
+++ cmd/default.go
@@ -1,9 +1,12 @@
 package cmd
 
-import "github.com/shomali11/slacker"
+import (
+	"github.com/evandroflores/claimr/messages"
+	"github.com/shomali11/slacker"
+)
 
 // Default command will be called when a command is not recognized.
 func Default(request *slacker.Request, response slacker.ResponseWriter) {
 	response.Typing()
-	response.Reply(Messages["command-not-found"])
+	response.Reply(messages.Messages["command-not-found"])
 }
