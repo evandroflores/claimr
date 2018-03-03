fc4fa8322901ca14271a264f7e1447725e000dea
Evandro Flores <eof@eof.com.br>
2018-03-03 19:07:01 +0000

    Migrating cmd/base

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -30,21 +30,21 @@ func CommandList() []model.Command {
 
 func isDirect(channelID string) (bool, error) {
 	if strings.HasPrefix(strings.ToUpper(channelID), directMessagePrefix) {
-		return true, fmt.Errorf(messages.Messages["direct-not-allowed"])
+		return true, fmt.Errorf(messages.Get("direct-not-allowed"))
 	}
 	return false, nil
 }
 
 func hasUserOnText(message string) (bool, error) {
 	if strings.Contains(strings.ToUpper(message), userPrefix) {
-		return true, fmt.Errorf(messages.Messages["shouldnt-mention-user"])
+		return true, fmt.Errorf(messages.Get("shouldnt-mention-user"))
 	}
 	return false, nil
 }
 
 func hasChannelOnText(message string) (bool, error) {
 	if strings.Contains(strings.ToUpper(message), channelPrefix) {
-		return true, fmt.Errorf(messages.Messages["shouldnt-mention-channel"])
+		return true, fmt.Errorf(messages.Get("shouldnt-mention-channel"))
 	}
 	return false, nil
 }
