52b5655bb3ddb431937c1d80ca08479c3c4e59aa
Evandro Flores <eof@eof.com.br>
2018-03-11 16:01:56 +0000

    migrate cmd/remove

--- cmd/remove.go
+++ cmd/remove.go
@@ -17,11 +17,13 @@ func remove(request *slacker.Request, response slacker.ResponseWriter) {
 	response.Typing()
 
 	event := getEvent(request)
-	if direct, err := isDirect(event.Channel); direct {
+	containerName := request.Param("container-name")
+
+	err := validateInput(event.Channel, containerName)
+	if err != nil {
 		response.Reply(err.Error())
 		return
 	}
-	containerName := request.Param("container-name")
 
 	container, err := model.GetContainer(event.Team, event.Channel, containerName)
 
